bb69c879df7a8550bbf6821d1ee136aae03f6784
Lawrence Davis <lawrence@lrdiv.co>
2017-02-13 12:56:52 -0600

    Disable GA if current user is admin

--- app/mixins/google-pageview.js
+++ app/mixins/google-pageview.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export default Ember.Mixin.create({
+  currentUser: Ember.inject.service(),
+
   beforePageviewToGA: function (ga) {
 
   },
@@ -9,8 +11,9 @@ export default Ember.Mixin.create({
   pageviewToGA: Ember.on('didTransition', function(page, title) {
     var page = page ? page : this.get('url');
     var title = title ? title : this.get('url');
+    var admin = this.get('currentUser.user.admin');
 
-    if (Ember.get(ENV, 'googleAnalytics.webPropertyId') != null) {
+    if (Ember.get(ENV, 'googleAnalytics.webPropertyId') != null && !admin) {
       var trackerType = Ember.getWithDefault(ENV, 'googleAnalytics.tracker', 'analytics.js');
 
       if (trackerType === 'analytics.js') {
